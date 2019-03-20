f54efc28588276f66eb1d30af3103238eaceb1da
rimo <rimol0514@gmail.com>
2019-03-20 17:59:19 +0900

    Recode構造体の変更に伴う修正

--- src/recgen.cpp
+++ src/recgen.cpp
@@ -32,7 +32,7 @@ void generateRecode(int n, int depth, std::string folderPath) {
     std::random_device rnd; // 非決定的乱数生成器、これでメルセンヌ・ツイスタのシードを設定
     std::mt19937 mt(rnd());
 
-    std::vector<Board> boards(60 + 1); // 初期盤面 + 60手分
+    Recode recodes[60 + 1]; // 初期盤面 + 60手分
 
     // n回ループ
     for (int i = 0; i < n; ++i) {
@@ -41,17 +41,12 @@ void generateRecode(int n, int depth, std::string folderPath) {
         // 終局までの手数(最初から4つマスが埋まっているので、最大60)
         int turns = 60;
         // 初期
-        boards[0] = Board(Black, 0x0000000810000000ULL, 0x0000001008000000ULL);
+        recodes[0] = Recode(0x0000000810000000ULL, 0x0000001008000000ULL, 0);
         
         for (int j = 1; j <= 60; ++j) {
-            Board current = boards[j - 1];
-            Color c = current.c;
-            Bitboard p = current.bits[c];
-            Bitboard o = current.bits[~c];
+            const Recode current = recodes[j - 1];
 
-            // printBoard(current); // 0-59までの盤面を表示できたら正解
-
-            Bitboard moves = getMoves(p, o);
+            Bitboard moves = getMoves(current.p, current.o);
             // 打つ手がない！
             if (moves == 0ULL) {
                 // ２回連続パス=終局
@@ -62,11 +57,13 @@ void generateRecode(int n, int depth, std::string folderPath) {
                 }
                 else {
                     passed = true;
-                    boards[--j] = c == Black ? Board(~c, p, o) : Board(~c, o, p);
+                    // 手番を変える
+                    recodes[--j] = Recode(current.o, current.p, current.turn);
                     continue;
                 }
             }
             else {
+                Recode next(current.o, current.p, j);
                 if (j <= 60 - depth) {
                     // 打つ手をランダムに決める
                     int chosen = mt() % (popcount(moves));
@@ -74,12 +71,12 @@ void generateRecode(int n, int depth, std::string folderPath) {
                     while (chosen--) moves &= moves - 1ULL;
                     // 一番下以外のビットを消す
                     Bitboard sqbit = -moves & moves;
-                    Bitboard flip = getFlip(p, o, sqbit);
+                    Bitboard flip = getFlip(current.p, current.o, sqbit);
 
-                    p ^= flip | sqbit;
-                    o ^= flip;
+                    next.o ^= flip | sqbit;
+                    next.p ^= flip;
                 }
-                // 残り10マスからソルバーを使います
+                // 残りdepthマスからsolverを使います
                 else {
                     int minscore = 64;
                     Bitboard bestMove = 0ULL; // 適当
@@ -87,9 +84,9 @@ void generateRecode(int n, int depth, std::string folderPath) {
                     // 1手すすめて、相手から見たスコアを計算、それが最小になるように取る
                     while (moves) {
                         Bitboard sqbit = moves & -moves;
-                        Bitboard flip = getFlip(p, o, sqbit);
+                        Bitboard flip = getFlip(current.p, current.o, sqbit);
 
-                        int score = solve(o ^ flip, p ^ flip ^ sqbit);
+                        int score = solve(current.o ^ flip, current.p ^ flip ^ sqbit);
                         if (score < minscore) {
                             minscore = score;
                             bestMove = sqbit;
@@ -99,23 +96,22 @@ void generateRecode(int n, int depth, std::string folderPath) {
                         moves ^= sqbit;
                     }
 
-                    p ^= bestFlip | bestMove;
-                    o ^= bestFlip;
+                    next.o ^= bestFlip | bestMove;
+                    next.p ^= bestFlip;
                 }
 
-                boards[j] = c == Black ? Board(~c, p, o) : Board(~c, o, p);
+                recodes[j] = next;
                 passed = false;
             }
         }
 
-        // std::cout << turns << std::endl; // これ意味ある？（草）
-        result = popcount(boards[turns].bits[Black]) - popcount(boards[turns].bits[White]);
+        result = popcount(recodes[turns].p) - popcount(recodes[turns].o);
 
         // さっき作ったフォルダ内に保存していく
         // 初期盤面(0手目終了時)は入れない。
         for (int j = 1; j <= turns; ++j) {
-            Recode recode(boards[j], j, result);
-            ofss[j - 1].write(reinterpret_cast<char*>(&recode), sizeof(Recode));
+            recodes[j].result = result;
+            ofss[j - 1].write(reinterpret_cast<char*>(&recodes[j]), sizeof(Recode));
         }
     }
 }
