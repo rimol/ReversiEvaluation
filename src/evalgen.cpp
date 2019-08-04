fc1de3283f289f48d50245c1fef3d07256c959e4
Rimo <rimol0514@gmail.com>
2019-08-04 20:33:45 +0900

    Revert "評価値生成並列化"
    This reverts commit 59429571134a41226b27b35a728478f880d5a6c6.

--- src/evalgen.cpp
+++ src/evalgen.cpp
@@ -7,16 +7,14 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
-#include <mutex>
 #include <sstream>
-#include <thread>
 #include <time.h>
 #include <vector>
 
 #define FOREACH_FEATURE_VALUE(Statement)            \
     for (int i = 0; i < GroupNum; ++i) {            \
         for (int j = 0; j < EvalArrayLength; ++j) { \
-            auto &fv = featureValues[stage][i][j];  \
+            auto &fv = featureValues[i][j];         \
             Statement                               \
         }                                           \
     }
@@ -26,53 +24,55 @@
         int _g = Feature.group[i];                                            \
         Bitboard _p = (_recordEx).playerRotatedBB[Feature.rotationType[i]];   \
         Bitboard _o = (_recordEx).opponentRotatedBB[Feature.rotationType[i]]; \
-        auto &fv = featureValues[stage][_g][extract(_p, _o, _g)];             \
+        auto &fv = featureValues[_g][extract(_p, _o, _g)];                    \
         Statement                                                             \
     }
 
 // 各特徴の評価値、評価値の更新分、ステップサイズをまとめて持つ
 struct FeatureValue {
-    double weight = 0.0;
-    double update = 0.0;
+    double evalValue = 0.0;
+    double evalValueUpdate = 0.0;
     double stepSize = 0.0;
 };
 
-static FeatureValue featureValues[60][GroupNum][EvalArrayLength];
-static FeatureValue mobilityValue[60];
+static FeatureValue featureValues[GroupNum][EvalArrayLength];
+static FeatureValue mobilityValue;
 /*
     盤面を縦横4分割して、その分割した盤面の1つに対して、空きマスの数が偶数なら-1、奇数なら1となる特徴.
     偶数理論に対応するやつ. 終盤用の特徴になるはず. 序盤の精度が下がりそうで怖い（変に最適化されそうなので）
 */
-static FeatureValue parityValue[60];
+static FeatureValue parityValue;
 
 static inline void fillAllArraysAndVarialblesWithZero() {
-    std::fill((FeatureValue *)featureValues, (FeatureValue *)(featureValues + 60), FeatureValue());
-    std::fill((FeatureValue *)mobilityValue, (FeatureValue *)(mobilityValue + 60), FeatureValue());
-    std::fill((FeatureValue *)parityValue, (FeatureValue *)(parityValue + 60), FeatureValue());
+    std::fill((FeatureValue *)featureValues, (FeatureValue *)(featureValues + GroupNum), FeatureValue());
+    mobilityValue = FeatureValue();
+    parityValue = FeatureValue();
 }
 
 // y - e
-static inline double evalLoss(const RecordEx &recordEx, const int stage) {
+static inline double evalLoss(const RecordEx &recordEx) {
     double e = 0.0;
-    FOREACH_FEATURE_IN(recordEx, { e += fv.weight; })
-    e += (double)mobilityDiff(recordEx.playerRotatedBB[0], recordEx.opponentRotatedBB[0]) * mobilityValue[stage].weight;
-    e += (double)paritySum(recordEx.playerRotatedBB[0] | recordEx.opponentRotatedBB[0]) * parityValue[stage].weight;
+    FOREACH_FEATURE_IN(recordEx, { e += fv.evalValue; })
+    e += (double)mobilityDiff(recordEx.playerRotatedBB[0], recordEx.opponentRotatedBB[0]) * mobilityValue.evalValue;
+    e += (double)paritySum(recordEx.playerRotatedBB[0] | recordEx.opponentRotatedBB[0]) * parityValue.evalValue;
     return (double)recordEx.result - e;
 }
 
-static inline void applyUpdatesOfEvalValues(const int stage) {
+static inline void applyUpdatesOfEvalValues() {
     FOREACH_FEATURE_VALUE(
-        fv.weight += fv.update * fv.stepSize;
-        fv.update = 0.0;)
+        fv.evalValue += fv.evalValueUpdate * fv.stepSize;
+        fv.evalValueUpdate = 0.0;)
 
-    mobilityValue[stage].weight += mobilityValue[stage].update * mobilityValue[stage].stepSize;
-    parityValue[stage].weight += parityValue[stage].update * parityValue[stage].stepSize;
+    mobilityValue.evalValue += mobilityValue.evalValueUpdate * mobilityValue.stepSize;
+    parityValue.evalValue += parityValue.evalValueUpdate * parityValue.stepSize;
 
-    mobilityValue[stage].update = parityValue[stage].update = 0.0;
+    mobilityValue.evalValueUpdate = parityValue.evalValueUpdate = 0.0;
 }
 
 // 評価値を計算してファイルに保存し、実際の結果と最終的な評価値による予測値の分散を返す。
-static double calculateEvaluationValue(std::string recordFilePath, double beta, int stage) {
+static double calculateEvaluationValue(std::string recordFilePath, double beta) {
+    fillAllArraysAndVarialblesWithZero();
+
     // ファイルを何回も読むのは無駄なので最初に全部読み込む
     std::ifstream ifs(recordFilePath, std::ios::ate | std::ios::binary);
     if (!ifs.is_open()) {
@@ -102,7 +102,7 @@ static double calculateEvaluationValue(std::string recordFilePath, double beta, 
         fv.stepSize = std::min(beta / 50.0, beta / fv.stepSize);)
 
     // これやるの忘れてたああああああああ
-    mobilityValue[stage].stepSize = parityValue[stage].stepSize = beta / (double)M;
+    mobilityValue.stepSize = parityValue.stepSize = beta / (double)M;
 
     double prevSquaredLossSum = 0.0;
     long long loopCounter = 0;
@@ -112,13 +112,13 @@ static double calculateEvaluationValue(std::string recordFilePath, double beta, 
         double squaredLossSum = 0;
         for (const auto &recordEx : records) {
             // 残差
-            double loss = evalLoss(recordEx, stage);
+            double loss = evalLoss(recordEx);
             squaredLossSum += loss * loss;
             // このデータで出現する各特徴に対し更新分を加算していく
-            FOREACH_FEATURE_IN(recordEx, { fv.update += loss; })
+            FOREACH_FEATURE_IN(recordEx, { fv.evalValueUpdate += loss; })
 
-            mobilityValue[stage].update += loss * (double)mobilityDiff(recordEx.playerRotatedBB[0], recordEx.opponentRotatedBB[0]);
-            parityValue[stage].update += loss * (double)paritySum(recordEx.playerRotatedBB[0] | recordEx.opponentRotatedBB[0]);
+            mobilityValue.evalValueUpdate += loss * (double)mobilityDiff(recordEx.playerRotatedBB[0], recordEx.opponentRotatedBB[0]);
+            parityValue.evalValueUpdate += loss * (double)paritySum(recordEx.playerRotatedBB[0] | recordEx.opponentRotatedBB[0]);
         }
 
         double currentVariance = squaredLossSum / (double)M;
@@ -126,19 +126,18 @@ static double calculateEvaluationValue(std::string recordFilePath, double beta, 
         // 終了条件わからん
         // 「前回との差がピッタリ0」を条件にすると終わらない
         // -> １局面あたりの変化（の二乗）がXを下回ったら終了する
-        // 10e-4 から 10e-6にして生成すると逆に弱くなったｗ
-        constexpr double X = 10e-4;
+        constexpr double X = 10e-6;
         if (std::abs(squaredLossSum - prevSquaredLossSum) / (double)M < X) {
-            std::cout << stage << ": Done. variance: " << currentVariance << ", loop: " << loopCounter << " times" << std::endl;
+            std::cout << "Done. variance: " << currentVariance << ", loop: " << loopCounter << " times" << std::endl;
             return currentVariance;
         }
 
         prevSquaredLossSum = squaredLossSum;
 
-        applyUpdatesOfEvalValues(stage);
+        applyUpdatesOfEvalValues();
 
         if (++loopCounter % 10 == 0) {
-            std::cout << stage << ": current variance: " << currentVariance << ", loop: " << loopCounter << " times" << std::endl;
+            std::cout << "current variance: " << currentVariance << ", loop: " << loopCounter << " times" << std::endl;
         }
     }
 }
@@ -148,32 +147,18 @@ void generateEvaluationFiles(std::string recordsFolderPath, std::string outputFo
     // 分散を保存するファイルを作る
     std::ofstream vofs(addFileNameAtEnd(outputFolderPath, "variance", "txt"));
 
-    fillAllArraysAndVarialblesWithZero();
-
-    std::vector<std::thread> threads;
-    std::mutex mtx;
     // (1-60).binについてそれぞれ計算→保存
     for (int i = 60; i >= 1; --i) {
-        threads.emplace_back([&mtx, &vofs, recordsFolderPath, beta, i, outputFolderPath]() {
-            double variance = calculateEvaluationValue(addFileNameAtEnd(recordsFolderPath, std::to_string(i), "bin"), beta, i);
+        // ファイルパスを渡して計算させる
+        double variance = calculateEvaluationValue(addFileNameAtEnd(recordsFolderPath, std::to_string(i), "bin"), beta);
+        // 保存～
+        vofs << i << ".bin: " << variance << std::endl;
 
-            mtx.lock();
-            vofs << i << ".bin: " << variance << std::endl;
-            mtx.unlock();
-
-            std::ofstream ofs(addFileNameAtEnd(outputFolderPath, std::to_string(i), "bin"), std::ios::binary);
-            // マクロの弊害がここに..
-            const int stage = i;
-            // 評価値のみ保存したいので仕方なしループ
-            FOREACH_FEATURE_VALUE(
-                ofs.write((char *)&fv.weight, sizeof(double));)
-            ofs.write((char *)&mobilityValue[i].weight, sizeof(double));
-            ofs.write((char *)&parityValue[i].weight, sizeof(double));
-        });
-        threads.back().join();
+        std::ofstream ofs(addFileNameAtEnd(outputFolderPath, std::to_string(i), "bin"), std::ios::binary);
+        // 評価値のみ保存したいので仕方なしループ
+        FOREACH_FEATURE_VALUE(
+            ofs.write((char *)&fv.evalValue, sizeof(double));)
+        ofs.write((char *)&mobilityValue.evalValue, sizeof(double));
+        ofs.write((char *)&parityValue.evalValue, sizeof(double));
     }
-
-    // for (auto &t : threads) {
-    //     t.join();
-    // }
 }
